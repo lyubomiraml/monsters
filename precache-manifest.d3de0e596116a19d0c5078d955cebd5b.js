1e653cadb00860b93df3a868fea0b960cc3201c2
Lyubomira Lyubomirova <lyubomira.lyubomirova@fadata.eu>
2020-04-17 10:57:13 +0300

    Updates

---
+++ precache-manifest.d3de0e596116a19d0c5078d955cebd5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744e0a0a4c6cdc87949f1804c55f32ef",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "2d4426930c432f4f0a86",
+    "url": "/monsters/static/css/main.11319235.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monsters/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "2d4426930c432f4f0a86",
+    "url": "/monsters/static/js/main.12ec779c.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
